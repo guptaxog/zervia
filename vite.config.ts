ca4e1478eca21d72f81dae3954c80d48fade05be
Ojas Gupta <ojasgupta@Mac.lan>
2025-09-26 22:38:58 +0530

    feat: configure for custom domain zervia.co

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Set the base path for GitHub Pages: https://guptaxog.github.io/zervia/
-  base: '/zervia/',
+  // Set the base path for custom domain: https://zervia.co
+  base: '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
